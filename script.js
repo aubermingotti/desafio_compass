d06bb7cfaae127fe7d2479dc146f69eb2d919ee0
aubermingotti <mingottiauber22@gmail.com>
2022-01-04 18:28:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function logar() {}
-
-document.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    const btn = document.querySelector('#enviar')
-    btn.click()
-    alert('Enviado com Sucesso')
-  }
-})
