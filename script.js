7ff1b066e8a2f703dc9bab6b884bcc45a6f1c07e
aubermingotti <mingottiauber22@gmail.com>
2022-01-04 15:13:05 -0300

    JS envio do formulário

---
+++ script.js
@@ -0,0 +1,9 @@
+function logar() {}
+
+document.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    const btn = document.querySelector('#enviar')
+    btn.click()
+    alert('Enviado com Sucesso')
+  }
+})
